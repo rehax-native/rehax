981f1ca2155cf32a3cda550df6da626517242e6a
Denis <denis@Macbook22.local>
2022-08-04 12:11:26 +0200

    Guard imports with preprocesser defs

--- native-abstraction/rehax.h
+++ native-abstraction/rehax.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#ifdef REHAX_WITH_APPKIT
 #include "ui/appkit/components/view/View.h"
 #include "ui/appkit/components/button/Button.h"
 #include "ui/appkit/components/text/Text.h"
@@ -9,7 +10,9 @@
 #include "ui/appkit/components/layouts/StackLayout.h"
 #include "ui/appkit/components/layouts/FlexLayout.h"
 #include "ui/appkit/components/view/Gesture.h"
+#endif
 
+#ifdef REHAX_WITH_FLUXE
 #include "ui/fluxe/components/view/View.h"
 #include "ui/fluxe/components/button/Button.h"
 #include "ui/fluxe/components/text/Text.h"
@@ -19,11 +22,13 @@
 #include "ui/fluxe/components/layouts/StackLayout.h"
 #include "ui/fluxe/components/layouts/FlexLayout.h"
 #include "ui/fluxe/components/view/Gesture.h"
+#endif
 
 #include "ui/base.h"
 
 namespace rehax::ui {
 
+  #ifdef REHAX_WITH_APPKIT
   namespace appkit {
     using View = impl::View;
     using Button = impl::Button;
@@ -36,6 +41,9 @@ namespace rehax::ui {
     using FlexLayout = impl::FlexLayout;
     using Gesture = impl::Gesture;
   }
+  #endif
+
+  #ifdef REHAX_WITH_FLUXE
   namespace fluxe {
     using View = impl::View;
     using Button = impl::Button;
@@ -48,4 +56,5 @@ namespace rehax::ui {
     using FlexLayout = impl::FlexLayout;
     using Gesture = impl::Gesture;
   }
+  #endif
 }
